c80ca92ce2bbc2fe7e2e6de0d4ebc788ca2abdec
zhanghe <zhanghe@tensorsecurity.cn>
2021-09-26 17:22:05 +0800

    优化图标代码

--- src/router/routers.js
+++ src/router/routers.js
@@ -1,5 +1,4 @@
 import Main from '@/components/main'
-import parentView from '@/components/parent-view'
 
 /**
  * iview-admin中meta除了原生参数外可配置的参数:
@@ -29,437 +28,194 @@ export default [
   },
   {
     path: '/',
-    name: '_home',
-    redirect: '/home',
+    name: 'home',
+    redirect: '/equipmentPurchaseManagement',
     component: Main,
-    meta: {
-      hideInMenu: true,
-      notCache: true
-    },
     children: [
       {
-        path: '/home',
-        name: 'home',
+        path: 'equipmentPurchaseManagement',
+        name: 'equipmentPurchaseManagement',
         meta: {
-          hideInMenu: true,
-          title: '首页',
-          notCache: true,
+          title: '设备购入管理',
           icon: 'md-home'
         },
-        component: () => import('@/view/single-page/home')
+        component: () => import('@/view/equipmentPurchaseManagement')
       }
     ]
   },
   {
-    path: '',
-    name: 'doc',
-    meta: {
-      title: '文档',
-      href: 'https://lison16.github.io/iview-admin-doc/#/',
-      icon: 'ios-book'
-    }
-  },
-  {
-    path: '/join',
-    name: 'join',
+    path: '/equipmentArchivesManagement',
+    name: 'equipmentArchivesManagement',
     component: Main,
-    meta: {
-      hideInBread: true
-    },
     children: [
       {
-        path: 'join_page',
-        name: 'join_page',
+        path: '/equipmentArchivesManagement',
+        name: 'equipmentArchivesManagement',
         meta: {
-          icon: '_qq',
-          title: 'QQ群'
+          icon: 'md-home',
+          title: '设备档案管理'
         },
-        component: () => import('@/view/join-page.vue')
+        component: () => import('@/view/blank.vue')
       }
     ]
   },
   {
-    path: '/message',
-    name: 'message',
+    path: '/assetBusinessManagement',
+    name: 'assetBusinessManagement',
     component: Main,
-    meta: {
-      hideInBread: true,
-      hideInMenu: true
-    },
     children: [
       {
-        path: 'message_page',
-        name: 'message_page',
+        path: '/assetBusinessManagement',
+        name: 'assetBusinessManagement',
         meta: {
-          icon: 'md-notifications',
-          title: '消息中心'
+          icon: 'md-home',
+          title: '资产业务管理'
         },
-        component: () => import('@/view/single-page/message/index.vue')
+        component: () => import('@/view/blank.vue')
       }
     ]
   },
   {
-    path: '/components',
-    name: 'components',
-    meta: {
-      icon: 'logo-buffer',
-      title: '组件'
-    },
+    path: '/machineMaintainableManagement',
+    name: 'machineMaintainableManagement',
     component: Main,
     children: [
       {
-        path: 'tree_select_page',
-        name: 'tree_select_page',
+        path: '/machineMaintainableManagement',
+        name: 'machineMaintainableManagement',
         meta: {
-          icon: 'md-arrow-dropdown-circle',
-          title: '树状下拉选择器'
+          icon: 'md-home',
+          title: '设备维修管理'
         },
-        component: () => import('@/view/components/tree-select/index.vue')
-      },
-      {
-        path: 'count_to_page',
-        name: 'count_to_page',
-        meta: {
-          icon: 'md-trending-up',
-          title: '数字渐变'
-        },
-        component: () => import('@/view/components/count-to/count-to.vue')
-      },
-      {
-        path: 'drag_list_page',
-        name: 'drag_list_page',
-        meta: {
-          icon: 'ios-infinite',
-          title: '拖拽列表'
-        },
-        component: () => import('@/view/components/drag-list/drag-list.vue')
-      },
-      {
-        path: 'drag_drawer_page',
-        name: 'drag_drawer_page',
-        meta: {
-          icon: 'md-list',
-          title: '可拖拽抽屉'
-        },
-        component: () => import('@/view/components/drag-drawer')
-      },
-      {
-        path: 'org_tree_page',
-        name: 'org_tree_page',
-        meta: {
-          icon: 'ios-people',
-          title: '组织结构树'
-        },
-        component: () => import('@/view/components/org-tree')
-      },
-      {
-        path: 'tree_table_page',
-        name: 'tree_table_page',
-        meta: {
-          icon: 'md-git-branch',
-          title: '树状表格'
-        },
-        component: () => import('@/view/components/tree-table/index.vue')
-      },
-      {
-        path: 'cropper_page',
-        name: 'cropper_page',
-        meta: {
-          icon: 'md-crop',
-          title: '图片裁剪'
-        },
-        component: () => import('@/view/components/cropper/cropper.vue')
-      },
-      {
-        path: 'tables_page',
-        name: 'tables_page',
-        meta: {
-          icon: 'md-grid',
-          title: '多功能表格'
-        },
-        component: () => import('@/view/components/tables/tables.vue')
-      },
-      {
-        path: 'split_pane_page',
-        name: 'split_pane_page',
-        meta: {
-          icon: 'md-pause',
-          title: '分割窗口'
-        },
-        component: () => import('@/view/components/split-pane/split-pane.vue')
-      },
-      {
-        path: 'markdown_page',
-        name: 'markdown_page',
-        meta: {
-          icon: 'logo-markdown',
-          title: 'Markdown编辑器'
-        },
-        component: () => import('@/view/components/markdown/markdown.vue')
-      },
-      {
-        path: 'editor_page',
-        name: 'editor_page',
-        meta: {
-          icon: 'ios-create',
-          title: '富文本编辑器'
-        },
-        component: () => import('@/view/components/editor/editor.vue')
-      },
-      {
-        path: 'icons_page',
-        name: 'icons_page',
-        meta: {
-          icon: '_bear',
-          title: '自定义图标'
-        },
-        component: () => import('@/view/components/icons/icons.vue')
+        component: () => import('@/view/blank.vue')
       }
     ]
   },
   {
-    path: '/update',
-    name: 'update',
-    meta: {
-      icon: 'md-cloud-upload',
-      title: '数据上传'
-    },
+    path: '/equipmentUsageManagement',
+    name: 'equipmentUsageManagement',
     component: Main,
     children: [
       {
-        path: 'update_table_page',
-        name: 'update_table_page',
+        path: '/equipmentUsageManagement',
+        name: 'equipmentUsageManagement',
         meta: {
-          icon: 'ios-document',
-          title: '上传Csv'
+          icon: 'md-home',
+          title: '设备使用管理'
         },
-        component: () => import('@/view/update/update-table.vue')
-      },
-      {
-        path: 'update_paste_page',
-        name: 'update_paste_page',
-        meta: {
-          icon: 'md-clipboard',
-          title: '粘贴表格数据'
-        },
-        component: () => import('@/view/update/update-paste.vue')
+        component: () => import('@/view/blank.vue')
       }
     ]
   },
   {
-    path: '/excel',
-    name: 'excel',
-    meta: {
-      icon: 'ios-stats',
-      title: 'EXCEL导入导出'
-    },
+    path: '/vendorInformationManagement',
+    name: 'vendorInformationManagement',
     component: Main,
     children: [
       {
-        path: 'upload-excel',
-        name: 'upload-excel',
+        path: '/vendorInformationManagement',
+        name: 'vendorInformationManagement',
         meta: {
-          icon: 'md-add',
-          title: '导入EXCEL'
+          icon: 'md-home',
+          title: '厂商信息管理'
         },
-        component: () => import('@/view/excel/upload-excel.vue')
-      },
-      {
-        path: 'export-excel',
-        name: 'export-excel',
-        meta: {
-          icon: 'md-download',
-          title: '导出EXCEL'
-        },
-        component: () => import('@/view/excel/export-excel.vue')
+        component: () => import('@/view/blank.vue')
       }
     ]
   },
   {
-    path: '/tools_methods',
-    name: 'tools_methods',
-    meta: {
-      hideInBread: true
-    },
+    path: '/statisticalAnalysisManagement',
+    name: 'statisticalAnalysisManagement',
     component: Main,
     children: [
       {
-        path: 'tools_methods_page',
-        name: 'tools_methods_page',
+        path: '/statisticalAnalysisManagement',
+        name: 'statisticalAnalysisManagement',
         meta: {
-          icon: 'ios-hammer',
-          title: '工具方法',
-          beforeCloseName: 'before_close_normal'
+          icon: 'md-home',
+          title: '统计分析管理'
         },
-        component: () => import('@/view/tools-methods/tools-methods.vue')
+        component: () => import('@/view/blank.vue')
       }
     ]
   },
   {
-    path: '/i18n',
-    name: 'i18n',
-    meta: {
-      hideInBread: true
-    },
+    path: '/lifeFirstAid(IOT)',
+    name: 'lifeFirstAid(IOT)',
     component: Main,
     children: [
       {
-        path: 'i18n_page',
-        name: 'i18n_page',
+        path: '/lifeFirstAid(IOT)',
+        name: 'lifeFirstAid(IOT)',
         meta: {
-          icon: 'md-planet',
-          title: 'i18n - {{ i18n_page }}'
+          icon: 'md-home',
+          title: '生命急救（物联）'
         },
-        component: () => import('@/view/i18n/i18n-page.vue')
+        component: () => import('@/view/blank.vue')
       }
     ]
   },
   {
-    path: '/error_store',
-    name: 'error_store',
-    meta: {
-      hideInBread: true
-    },
+    path: '/largeEquipment(IOT)',
+    name: 'largeEquipment(IOT)',
     component: Main,
     children: [
       {
-        path: 'error_store_page',
-        name: 'error_store_page',
+        path: '/largeEquipment(IOT)',
+        name: 'largeEquipment(IOT)',
         meta: {
-          icon: 'ios-bug',
-          title: '错误收集'
+          icon: 'md-home',
+          title: '大型装备（物联）'
         },
-        component: () => import('@/view/error-store/error-store.vue')
+        component: () => import('@/view/blank.vue')
       }
     ]
   },
   {
-    path: '/error_logger',
-    name: 'error_logger',
-    meta: {
-      hideInBread: true,
-      hideInMenu: true
-    },
+    path: '/dataIntegrationManagement',
+    name: 'dataIntegrationManagement',
     component: Main,
     children: [
       {
-        path: 'error_logger_page',
-        name: 'error_logger_page',
+        path: '/dataIntegrationManagement',
+        name: 'dataIntegrationManagement',
         meta: {
-          icon: 'ios-bug',
-          title: '错误收集'
+          icon: 'md-home',
+          title: '数据集成管理'
         },
-        component: () => import('@/view/single-page/error-logger.vue')
+        component: () => import('@/view/blank.vue')
       }
     ]
   },
   {
-    path: '/directive',
-    name: 'directive',
-    meta: {
-      hideInBread: true
-    },
+    path: '/regionalMapManagement',
+    name: 'regionalMapManagement',
     component: Main,
     children: [
       {
-        path: 'directive_page',
-        name: 'directive_page',
+        path: '/regionalMapManagement',
+        name: 'regionalMapManagement',
         meta: {
-          icon: 'ios-navigate',
-          title: '指令'
+          icon: 'md-home',
+          title: '区域地图管理'
         },
-        component: () => import('@/view/directive/directive.vue')
+        component: () => import('@/view/blank.vue')
       }
     ]
   },
   {
-    path: '/multilevel',
-    name: 'multilevel',
-    meta: {
-      icon: 'md-menu',
-      title: '多级菜单'
-    },
+    path: '/basicSystemManagement',
+    name: 'basicSystemManagement',
     component: Main,
     children: [
       {
-        path: 'level_2_1',
-        name: 'level_2_1',
+        path: '/basicSystemManagement',
+        name: 'basicSystemManagement',
         meta: {
-          icon: 'md-funnel',
-          title: '二级-1'
+          icon: 'md-home',
+          title: '系统基础管理'
         },
-        component: () => import('@/view/multilevel/level-2-1.vue')
-      },
-      {
-        path: 'level_2_2',
-        name: 'level_2_2',
-        meta: {
-          access: ['super_admin'],
-          icon: 'md-funnel',
-          showAlways: true,
-          title: '二级-2'
-        },
-        component: parentView,
-        children: [
-          {
-            path: 'level_2_2_1',
-            name: 'level_2_2_1',
-            meta: {
-              icon: 'md-funnel',
-              title: '三级'
-            },
-            component: () => import('@/view/multilevel/level-2-2/level-2-2-1.vue')
-          },
-          {
-            path: 'level_2_2_2',
-            name: 'level_2_2_2',
-            meta: {
-              icon: 'md-funnel',
-              title: '三级'
-            },
-            component: () => import('@/view/multilevel/level-2-2/level-2-2-2.vue')
-          }
-        ]
-      },
-      {
-        path: 'level_2_3',
-        name: 'level_2_3',
-        meta: {
-          icon: 'md-funnel',
-          title: '二级-3'
-        },
-        component: () => import('@/view/multilevel/level-2-3.vue')
-      }
-    ]
-  },
-  {
-    path: '/argu',
-    name: 'argu',
-    meta: {
-      hideInMenu: true
-    },
-    component: Main,
-    children: [
-      {
-        path: 'params/:id',
-        name: 'params',
-        meta: {
-          icon: 'md-flower',
-          title: route => `{{ params }}-${route.params.id}`,
-          notCache: true,
-          beforeCloseName: 'before_close_normal'
-        },
-        component: () => import('@/view/argu-page/params.vue')
-      },
-      {
-        path: 'query',
-        name: 'query',
-        meta: {
-          icon: 'md-flower',
-          title: route => `{{ query }}-${route.query.id}`,
-          notCache: true
-        },
-        component: () => import('@/view/argu-page/query.vue')
+        component: () => import('@/view/blank.vue')
       }
     ]
   },
---
+++ src/view/equipmentPurchaseManagement/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="equipment-purchase-management"></div>
+</template>
+
+<script>
+export default {
+  name: 'equipmentPurchaseManagement',
+  data () {
+    return {}
+  }
+}
+</script>
+<style></style>
